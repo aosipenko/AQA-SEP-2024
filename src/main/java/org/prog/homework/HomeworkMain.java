3a92818ec3b887bfb018caeb0690a5f3f3f33e9b
sergi <sergienkoruslan74@gmail.com>
2024-09-26 15:27:02 -0300

    leson2

--- src/main/java/org/prog/homework/HomeworkMain.java
+++ src/main/java/org/prog/homework/HomeworkMain.java
@@ -3,6 +3,31 @@ package org.prog.homework;
 public class HomeworkMain {
 
     public static void main(String[] args) {
+        Airplane airplane = new Airplane();
+        Airplane airline = airplane;
+        airplane.airline = "Condor";
+
+
+        Airplane number = airplane;
+        airplane.number = "EEEE";
+        System.out.println();
+        System.out.println("flight Airplane number is" + number.number);
+        System.out.println("flight Airplane airline is" + number.airline);
+        airplane.weFly();
+
+        airplane.weFly(" Halifax ", " Halifax ",
+                " Frankfurt ", " Paris ");
+
+
+
+        System.out.println(number.equals(airline));
+        System.out.println(airline.equals(number));
+
+        Object o = new Object();
+        o.hashCode();
 
     }
 }
+
+
+
