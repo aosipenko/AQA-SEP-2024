88e00820c80507794f4996a6338a8220df2bca4e
sergi <sergienkoruslan74@gmail.com>
2024-09-28 16:40:56 -0300

    homework3

--- src/main/java/org/prog/exceptions/ExceptionsHomework.java
+++ src/main/java/org/prog/exceptions/ExceptionsHomework.java
@@ -1,5 +1,8 @@
 package org.prog.exceptions;
 
+import javax.imageio.IIOException;
+import java.io.*;
+
 //TODO: write method that will count text file lines and print it (put it in separate method)
 // (you can use ChatGPT to generate this part)
 // Trasnform FileNotFoundException into RuntimeException
@@ -9,6 +12,23 @@ package org.prog.exceptions;
 public class ExceptionsHomework {
 
     public static void main(String[] args) {
-
+        FileReader("C:\\Users\\sergi\\IdeaProjects\\AQA-SEP-2024\\README.md");
+        FileReader("C:\\Users\\sergi\\IdeaProjects\\AQA-SEP-202\\README.md");
     }
+
+    public static void FileReader(String fileName) {
+        try {
+            File file = new File(fileName);
+            if (!file.exists()) {
+                throw new FileNotFoundException("File not found:" + fileName);
+            }
+            System.out.println("File" + fileName + "exists");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Runtime exception:" + e.getMessage(), e);
+        }
+            System.out.println(" YAY ");
+    }
+
 }
+
+
