3a49657e2d6f7f17e8613ac1bb019fa59613c18f
s0109 <s01091987@gmail.com>
2024-10-04 15:06:18 +0300

    home1

--- src/main/java/org/prog/homework/Airplane.java
+++ src/main/java/org/prog/homework/Airplane.java
@@ -5,4 +5,20 @@ package org.prog.homework;
 //TODO: add overload flyTo: destination, startAriport, transitAirport and print:
 // Flight (flightNumber) of comapny (comapny) from to through
 public class Airplane {
+    public String airCompany;
+    public int flightNumber;
+    @Override
+    public int hashCode() {
+        return airCompany.hashCode() + flightNumber;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if(object instanceof  Airplane) {
+           if(this.airCompany == ((Airplane) object).airCompany && this.flightNumber == ((Airplane) object).flightNumber){
+               return true;
+           } return false;
+        } else return false;
+    }
+
 }
--- src/main/java/org/prog/homework/HomeworkMain.java
+++ src/main/java/org/prog/homework/HomeworkMain.java
@@ -3,6 +3,18 @@ package org.prog.homework;
 public class HomeworkMain {
 
     public static void main(String[] args) {
-
+        Airplane plane1 = new Airplane();
+        plane1.airCompany = "Lufthansa";
+        plane1.flightNumber = 98;
+        Airplane plane2 = new Airplane();
+        plane2.airCompany = "Delta Air Lines";
+        plane2.flightNumber = 65;
+        Airplane plane3 = new Airplane();
+        plane3.airCompany = "Delta Air Lines";
+        plane3.flightNumber = 65;
+        System.out.println(plane1.hashCode());
+        System.out.println(plane2.hashCode());
+        System.out.println(plane1.equals(plane2));
+        System.out.println(plane2.equals(plane3));
     }
 }
